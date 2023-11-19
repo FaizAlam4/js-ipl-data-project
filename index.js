434e5d1d4c40a3db85c3f9fcc6ef51ddc08b727c
FaizAlam4 <faizalam4e@gmail.com>
2023-11-19 17:55:07 +0530

    completed highest dismissal broken part

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ import { matchesPerYear } from "./src/server/matchesPerYear.js";
 import { matchesWinPerYear } from "./src/server/matchesWinPerYear.js";
 import { mostPOM } from "./src/server/mostPOM.js";
 import { strikeRate } from "./src/server/strikeRate.js";
-import  {superOver}  from "./src/server/superOver.js";
+import { superOver } from "./src/server/superOver.js";
 import { winTossWinMatch } from "./src/server/winTossWinMatch.js";
-import {highestDismissal} from "./src/server/highestDismissal.js"
+import { highestDismissal } from "./src/server/highestDismissal.js";
 
-import pkg from 'readline-sync';
+import pkg from "readline-sync";
 
-let matchFilePath="./src/data/match.json";
-let deliveryFilePath="./src/data/delivery.json";
-
+let matchFilePath = "./src/data/match.json";
+let deliveryFilePath = "./src/data/delivery.json";
 
 // superOver(matchFilePath,deliveryFilePath,'./src/public/output/superOver.json');
 // winTossWinMatch(matchFilePath,deliveryFilePath,'./src/public/output/winTossWinMatch.json');
@@ -23,7 +22,11 @@ let deliveryFilePath="./src/data/delivery.json";
 // economicalBowler(matchFilePath,deliveryFilePath,'./src/public/output/economicalBowlers.json');
 highestDismissal(matchFilePath,deliveryFilePath,'./src/public/output/highestDismissal.json');
 
-
 //---- Taking argument from console for strikeRate -----
-// let batsmanName=pkg.question("Enter the batsman:")
-// strikeRate(matchFilePath,deliveryFilePath,batsmanName,'./src/public/output/strikeRate.json');
+// let batsmanName = pkg.question("Enter the batsman:");
+// strikeRate(
+//   matchFilePath,
+//   deliveryFilePath,
+//   batsmanName,
+//   "./src/public/output/strikeRate.json"
+// );
--- src/server/highestDismissal.js
+++ src/server/highestDismissal.js
@@ -1,13 +1,18 @@
+import { count } from "console";
 import fs from "fs";
 
-export let highestDismissal = (matchPath, deliveryPath, outputPath) => {
+export const highestDismissal = (matchPath, deliveryPath, outputPath) => {
   let deliveryData = fs.readFileSync(deliveryPath, "utf-8");
   deliveryData = JSON.parse(deliveryData);
+  
 
   // let matchData = fs.readFileSync(matchPath, "utf-8");
   // matchData = JSON.parse(matchData);
+  let finalObject = { 'batsman': {'name':''}, 'dismissed by':{'bowler': "", 'count': 0} };
+
+  console.log(finalObject['count'])
   let arr = {};
-  let acc=deliveryData.reduce((acc, obj) => {
+  arr = deliveryData.reduce((acc, obj) => {
     if (obj.player_dismissed != "") {
       if (acc[obj.batsman] == undefined) {
         acc[obj.batsman] = {};
@@ -16,19 +21,45 @@ export let highestDismissal = (matchPath, deliveryPath, outputPath) => {
       if (acc[obj.batsman][obj.bowler] == undefined) {
         acc[obj.batsman][obj.bowler] = 0;
       }
+
       acc[obj.batsman][obj.bowler]++;
+
+     
+      if (acc[obj.batsman][obj.bowler] > finalObject['dismissed by']['count']) {
+        finalObject['dismissed by']['count']=acc[obj.batsman][obj.bowler];
+        finalObject['batsman']['name']=obj.batsman;
+        finalObject['dismissed by']['bowler']=obj.bowler;
+      }
+      else if(acc[obj.batsman][obj.bowler] == finalObject['dismissed by']['count']){
+        finalObject['batsman']['name']=obj.batsman;
+        finalObject['dismissed by']['bowler']=obj.bowler;
+      }
     }
     return acc;
-  });
+  },{});
 
-  let finalResult={};
-  acc = Object.entries(acc);
-  console.log(acc);
-  for(let element of acc){
-    let data=Object.entries(element[1]);
-    data.sort((a,b)=>{return a[1]-b[1]});
-    
-  }
+ console.log(finalObject);
+  // arr = Object.entries(arr);
+  // console.log(arr);
+  // for (let element of arr) {
+  //   //  console.log(element)
+  //   const data = Object.entries(element[1]);
+  //   data.sort((a, b) => {
+  //     return b[1] - a[1];
+  //   });
+  //   // console.log(data)
+  //   if (finalResult[element[0]] == undefined) {
+  //     finalResult[element[0]] = {};
+  //   }
+  //   if (finalResult[element[0]][0] == undefined) {
+  //     finalResult[element[0]][data] = 0;
+
+  //     //   }
+  //     finalResult[element[0]][data] = data[0][1];
+  //   }
+  // }
+
+  // console.log(finalResult)
 
   // for(let key in )
   // arr.map((element)=>{
